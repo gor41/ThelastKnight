4985b30ba0d6353be166140d3c1e7c9cff4c42cf
gor41 <matveihohorin@yandex.ru>
2024-05-16 20:33:43 +0300

    8

--- Assets/Scripts/Enemy/SceletonAI.cs
+++ Assets/Scripts/Enemy/SceletonAI.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class SceletonAI : MonoBehaviour
 {
+    [Header("Floats")]
     public float SceletSpeed;
+    public float AttackRadiusScelet;
+
+    [Header("GameObjects and other")]
     public LayerMask PlayerIsLayer;
     public GameObject AttackBoxScelet;
     public Transform Startpoint;
+    public Transform AttakcRadiusPointScelet;
+    
+
     private Animator animator;
     private Transform player;
+
+
     private bool isFlipped = false;
+    private bool isCanAttack = false;
+    private bool isAttack = false;
+    public bool isKdAttack = false;
 
     private void Awake()
     {
@@ -26,6 +38,7 @@ public class SceletonAI : MonoBehaviour
     {
         LookToPlayer();
         MoveScelet();
+        AttackScelet();
     }
     public void LookToPlayer()
     {
@@ -49,17 +62,51 @@ public class SceletonAI : MonoBehaviour
     {
         Vector2 targetPlayer = new Vector2(player.position.x, transform.position.y);
         Vector2 tarhetStartPoint = new Vector2(Startpoint.position.x, transform.position.y);
-        if (Vector2.Distance(transform.position, player.position) < 5)
+        if (Vector2.Distance(transform.position, player.position) < 5 && isAttack == false)
         {
             transform.position = Vector2.MoveTowards(transform.position, targetPlayer, SceletSpeed * Time.deltaTime);
+            animator.SetBool("isRunScl", true);
         }
-        else
+        else if(Vector2.Distance(transform.position,player.position) > 5 && isAttack == false)
         {
             transform.position = Vector2.MoveTowards(transform.position, tarhetStartPoint, SceletSpeed * Time.deltaTime);
+        }
+        else if(Vector2.Distance(transform.position, tarhetStartPoint) > 0.1f && isAttack == false)
+        {
+            animator.SetBool("isRunScl", false);
         }
     }
     public void AttackScelet()
     {
+        isCanAttack = Physics2D.OverlapCircle(AttakcRadiusPointScelet.position, AttackRadiusScelet, PlayerIsLayer);
+        if(isCanAttack == true && isAttack == false && isKdAttack == false)
+        {
+            StartCoroutine(AttackSceletCoroutine());
+            StartCoroutine(KDAttack());
+            animator.SetBool("isAttackScl", true);
+        }
+        else
+        {
+            animator.SetBool("isAttackScl", false);
+        }
+
+
+
+
+    }
+    IEnumerator AttackSceletCoroutine()
+    {
+        AttackBoxScelet.SetActive(true);
+        isAttack = true;
+        yield return new WaitForSeconds(0.2f);
+        AttackBoxScelet.SetActive(false);
+        isAttack = false;
+    }
+    IEnumerator KDAttack()
+    {
+        isKdAttack = true;
+        yield return new WaitForSeconds(0.5f);
+        isKdAttack = false;
 
     }
 
